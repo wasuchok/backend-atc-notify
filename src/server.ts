52ff661edfe2514490462161edafb0651630dec6
wasuchok <116450019+wasuchok@users.noreply.github.com>
2025-11-24 16:13:18 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
+import cors from 'cors';
 import express from "express";
 import authRoutes from "./Routes/AuthRoutes";
 
 const app = express()
 
 app.use(express.json());
+app.use(cors())
 
 const port = 3300
 
